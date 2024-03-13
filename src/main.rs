5a1f8914c397c8d6ec0ca0a9256867c90d4116ad
Laysson Luz <layssonoliveir4@gmail.com>
2024-03-13 21:28:17 +0000

    First xml reader version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+pub mod reader;
+
 fn main() {
     println!("Hello, world!");
+
+    let xml = r#"<tag1 att1 = "test">
+                <tag2><!--Test comment-->Test</tag2>
+                <tag2>Test 2</tag2>
+             </tag1>"#;
+
+    reader::read_xml(&xml);
 }
---
+++ src/reader.rs
@@ -0,0 +1,37 @@
+use quick_xml::events::Event;
+use quick_xml::reader::Reader;
+
+pub fn read_xml(xml: &str) {
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut count = 0;
+    let mut txt = Vec::new();
+    let mut buf = Vec::new();
+    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
+    loop {
+        // NOTE: this is the generic case when we don't know about the input BufRead.
+        // when the input is a &str or a &[u8], we don't actually need to use another
+        // buffer, we could directly call `reader.read_event()`
+        match reader.read_event_into(&mut buf) {
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+            // exits the loop when reaching end of file
+            Ok(Event::Eof) => break,
+
+            Ok(Event::Start(e)) => match e.name().as_ref() {
+                b"tag1" => println!(
+                    "attributes values: {:?}",
+                    e.attributes().map(|a| a.unwrap().value).collect::<Vec<_>>()
+                ),
+                b"tag2" => count += 1,
+                _ => (),
+            },
+            Ok(Event::Text(e)) => txt.push(e.unescape().unwrap().into_owned()),
+
+            // There are several other `Event`s we do not consider here
+            _ => (),
+        }
+        // if we don't keep a borrow elsewhere, we can clear the buffer to keep memory usage low
+        buf.clear();
+    }
+}
